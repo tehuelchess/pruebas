bd34086e1ae613d942315a5a6370dfb5c0fca5d8
Nicolas Silva <nsilva@minsegpres.gob.cl>
2012-07-03 16:02:47 -0400

    Asistencia para reglas en conexiones

--- application/views/backend/procesos/ajax_editar_conexion.php
+++ application/views/backend/procesos/ajax_editar_conexion.php
@@ -1,3 +1,13 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+        //Funcionalidad del llenado de nombre usando el boton de asistencia
+        $("#formEditarConexion .asistencia .dropdown-menu a").click(function(){
+            var nombre=$(this).text();
+            $("#formEditarConexion input[name=regla]").val(nombre);
+        });
+    });
+</script>
+
 <div class="modal-header">
     <a class="close" data-dismiss="modal">×</a>
     <h3>Editar Conexión</h3>
@@ -19,7 +29,15 @@
         <?php if($conexion->tipo=='evaluacion' || $conexion->tipo=='paralelo_evaluacion'):?>
         <label>Regla</label>
         <p class="help-block">Los nombres de campos escribalos anteponiendo @@. Ej: @@edad >= 18</p>
-        <input type="text" name="regla" value="<?=htmlspecialchars($conexion->regla)?>" /> 
+        <input type="text" name="regla" value="<?=htmlspecialchars($conexion->regla)?>" />
+        <div class="btn-group asistencia" style="display: inline-block; vertical-align: top;">
+            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-th-list"></i><span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <?php foreach ($conexion->TareaOrigen->Proceso->getCampos() as $c): ?>
+                    <li><a href="#">@@<?= $c->nombre ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
         <?php endif; ?>
     </form>
 </div>
