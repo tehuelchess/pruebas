63b9a6d5c00ec8acee2aff1386e2e637f33a2909
Nicolas Silva <nsilva@minsegpres.gob.cl>
2012-07-20 11:51:08 -0400

    Se previene html injection y se limpia codigo de campofile

--- application/models/campo_file.php
+++ application/models/campo_file.php
@@ -7,10 +7,10 @@ class CampoFile extends Campo {
     protected function display($modo, $dato) {   
         $display='<label>' . $this->etiqueta . (in_array('required', $this->validacion) ? '' : ' (Opcional)') . '</label>';
         $display.='<div>';
-        $display.='<div class="' . ($modo == 'visualizacion' ? '' : 'file-uploader') . '" name="' . $this->nombre . '" value="' . ($dato ? $dato->valor : '') . '"></div>';
-        $display.='<input type="hidden" name="' . $this->nombre . '" value="' . ($dato ? $dato->valor : '') . '" />';
+        $display.='<div class="' . ($modo == 'visualizacion' ? '' : 'file-uploader') . '"></div>';
+        $display.='<input type="hidden" name="' . $this->nombre . '" value="' . ($dato ? htmlspecialchars($dato->valor) : '') . '" />';
         if ($dato)
-            $display.='<p><a href="' . site_url('uploader/datos_get/' . $dato->valor) . '" target="_blank">' . $dato->valor . '</a></p>';
+            $display.='<p><a href="' . site_url('uploader/datos_get/' . htmlspecialchars ($dato->valor)) . '" target="_blank">' . htmlspecialchars ($dato->valor) . '</a></p>';
         $display.='</div>';
 
         return $display;
