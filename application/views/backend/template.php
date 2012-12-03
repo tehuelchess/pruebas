838f021d7ae04d42b7edb7c148019cf369424ab7
Nicolas Silva <nsilva@minsegpres.gob.cl>
2012-12-03 10:14:55 -0300

    Se quitan lineas inutiles

--- application/views/backend/template.php
+++ application/views/backend/template.php
@@ -59,9 +59,6 @@
 
         <link rel="shortcut icon" href="<?= base_url() ?>assets/ico/favicon.ico">
 
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= base_url() ?>assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= base_url() ?>assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="<?= base_url() ?>assets/ico/apple-touch-icon-57-precomposed.png">
     </head>
     <body>
         <div class="container-fluid">
