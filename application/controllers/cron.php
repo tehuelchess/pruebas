fc4d8541361cd24cc86d29efe2a8768cf05d7347
Nicolas Silva <nsilva@minsegpres.gob.cl>
2012-12-05 11:29:08 -0300

    Fix: No estaba funcionando el cron

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Cron extends MY_Controller {
+class Cron extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
