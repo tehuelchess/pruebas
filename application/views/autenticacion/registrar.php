8818ef2321df03328fe4ad93464db15405916474
Nicolas Silva <nsilva@minsegpres.gob.cl>
2012-12-18 11:40:15 -0300

    Se corrige registro del sistema en el frontend, dejando el correo como obligatorio.

--- application/views/autenticacion/registrar.php
+++ application/views/autenticacion/registrar.php
@@ -1,28 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <base href="<?= base_url() ?>" />
-        <meta charset="utf-8">
-        <title>Tramitador - Autenticación</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <!-- Le styles -->
-        <link href="assets/css/bootstrap.css" rel="stylesheet">
-        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="assets/css/common.css" rel="stylesheet">
-
-        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-
-        <!-- Le fav and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.ico">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+        <?php $this->load->view('head')?>
     </head>
 
     <body>
@@ -40,9 +19,9 @@
                             <input name="password" type="password" class="input-xlarge">
                             <label>Confirmar contraseña</label>
                             <input name="password_confirm" type="password" class="input-xlarge">
-                            <label>Correo electrónico (Opcional)</label>
+                            <label>Correo electrónico</label>
                             <input type="text" name="email" class="input-xlarge" />
-                            <p class="help-block">Importante para recibir notificaciones del estado de sus trámites.</p>
+                            <p class="help-block">En este correo recibiras notificaciones del estado de sus trámites.</p>
                             <div class="form-actions">
                                 <a class="btn" href="#" onclick="javascript:history.back();">Volver</a>
                                 <button class="btn btn-primary" type="submit">Ingresar</button>
@@ -57,12 +36,6 @@
 
 
 
-        <!-- Le javascript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="assets/js/jquery-ui/js/jquery-1.7.1.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/jquery.chosen/chosen.jquery.min.js"></script> <?php //Soporte para selects con multiple choices   ?>
-        <script src="assets/js/common.js"></script>
+
     </body>
 </html>
