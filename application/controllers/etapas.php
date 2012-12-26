bfba4ef04a2bd19480eb11b51e7d54751f27c31f
Nicolas Silva <nsilva@minsegpres.gob.cl>
2012-12-26 10:49:14 -0300

    fixes #54

--- application/controllers/etapas.php
+++ application/controllers/etapas.php
@@ -242,4 +242,20 @@ class Etapas extends MY_Controller {
         $this->load->view('template_iframe',$data);
     }
     
+    public function ver($etapa_id,$paso=0){
+        $etapa=Doctrine::getTable('Etapa')->find($etapa_id);
+        
+        if(UsuarioSesion::usuario()->id!=$etapa->usuario_id){
+            echo 'No tiene permisos para hacer seguimiento a este tramite.';
+            exit;
+        }
+        
+        $data['etapa']=$etapa;
+        $data['paso']=$paso;
+        
+        $data['title']='Historial - '.$etapa->Tarea->nombre;
+        $data['content']='etapas/ver';
+        $this->load->view('template',$data);
+    }
+    
 }
--- application/views/tramites/participados.php
+++ application/views/tramites/participados.php
@@ -8,23 +8,42 @@
             <th>Etapa actual</th>
             <th>Fecha Modificación</th>
             <th>Estado</th>
+            <th>Acciones</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($tramites as $t): ?>
             <tr>
-                <td><?=$t->id?></td>
+                <td><?= $t->id ?></td>
                 <td><?= $t->Proceso->nombre ?></td>
                 <td>
                     <?php
-                        $etapas_array=array();
-                        foreach($t->getEtapasActuales() as $e)
-                            $etapas_array[]=$e->Tarea->nombre;
-                        echo implode(', ',$etapas_array);
+                    $etapas_array = array();
+                    foreach ($t->getEtapasActuales() as $e)
+                        $etapas_array[] = $e->Tarea->nombre;
+                    echo implode(', ', $etapas_array);
                     ?>
                 </td>
-                <td><?= strftime('%c',mysql_to_unix($t->updated_at))?></td>
-                <td><?=$t->pendiente?'Pendiente':'Completado'?></td>
+                <td><?= strftime('%c', mysql_to_unix($t->updated_at)) ?></td>
+                <td><?= $t->pendiente ? 'Pendiente' : 'Completado' ?></td>
+                <td>
+                    <?php $etapas = $t->getEtapasParticipadas(UsuarioSesion::usuario()->id) ?>
+                    <?php if (count($etapas) == 3e4354) : ?>
+                        <a href="<?= site_url('etapas/ver/' . $etapas[0]->id) ?>" class="btn btn-primary">Ver historial</a>
+                    <?php else: ?>
+                        <div class="btn-group">
+                            <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
+                                Ver historial
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <?php foreach($etapas as $e):?>
+                                <li><a href="<?=site_url('etapas/ver/'.$e->id)?>"><?=$e->Tarea->nombre?></a></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                    <?php endif ?>
+                </td>
             </tr>
         <?php endforeach; ?>
     </tbody>
