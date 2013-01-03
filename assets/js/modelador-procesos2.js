3432c8d8aefd44e3a8b40415beab35663d86a938
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-03 16:03:32 -0300

    mejoras

--- assets/js/modelador-procesos2.js
+++ assets/js/modelador-procesos2.js
@@ -27,26 +27,59 @@ $(document).ready(function(){
             
             
             //Buscamos un id para asignarle
-            console.log(diagram.model.nodeDataArray);
             
             
-            var i=0;
-            while(true){
-                i++;
-                var id="box_"+i;
-                if($("#"+id).size()==0){   
-                    break;
-                }   
+            var id=1;
+            for (var i in diagram.model.nodeDataArray){
+                if(diagram.model.nodeDataArray[i].key>id)
+                    id=1+diagram.model.nodeDataArray[i].key;
             }
             
+            
             diagram.model.addNodeData({
-          key: id,
-          name: "Tarea",
-          loc: left+" "+top
-      });
+                key: id,
+                name: "Tarea",
+                loc: left+" "+top
+            });
             modo=null;
             $("#areaDibujo .botonera .createBox").removeClass("disabled");
             $.post(site_url+"backend/procesos/ajax_crear_tarea/"+procesoId+"/"+id,"nombre=Tarea&posx="+left+"&posy="+top);
+        }
+    });
+    
+    
+    diagram.addDiagramListener("ObjectSingleClicked", function(event) {
+        if(modo=="createConnection"){
+            elements.push(this.id);
+            if(elements.length==2){
+                var c=new Object();
+                c.tipo=tipo;
+                c.source=elements[0];
+                c.target=elements[1];
+                
+                //Validaciones
+                if(tipo=="secuencial" && jsPlumb.getConnections({source:c.source}).length){
+                    alert("Las conexiones secuenciales no pueden ir hacia mas de una tarea");
+                    return;
+                }
+                
+                
+                diagram.model.addLinkData({
+                    from:c.tipo,
+                    to:c.source,
+                    type:c.target
+                })
+                
+ 
+                
+                modo=null;
+                elements.length=0;
+                $("#areaDibujo .botonera .createConnection").removeClass("disabled");
+                $.post(site_url+"backend/procesos/ajax_crear_conexion/"+procesoId,"tarea_id_origen="+c.source+"&tarea_id_destino="+c.target+"&tipo="+c.tipo);
+                
+            }else{
+                $(this).addClass("selected");
+            }
         }
     });
  
