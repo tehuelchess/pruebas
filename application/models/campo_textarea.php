7c60986a084c181fd181296be6b2d3a84bbc5362
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-03 10:17:25 -0300

    Se aumenta tamano de textareas

--- application/models/campo_textarea.php
+++ application/models/campo_textarea.php
@@ -6,7 +6,7 @@ class CampoTextArea extends Campo{
 
     protected function display($modo, $dato) {       
         $display='<label>' . $this->etiqueta . (!$this->readonly && !in_array('required', $this->validacion) ? ' (Opcional)' : '') . '</label>';
-        $display.='<textarea ' . ($this->readonly || $modo == 'visualizacion' ? 'readonly' : '') . ' name="' . $this->nombre . '">' . ($dato?htmlspecialchars($dato->valor):'') . '</textarea>';
+        $display.='<textarea rows="5" class="input-xxlarge" ' . ($this->readonly || $modo == 'visualizacion' ? 'readonly' : '') . ' name="' . $this->nombre . '">' . ($dato?htmlspecialchars($dato->valor):'') . '</textarea>';
     
         return $display;
     }
