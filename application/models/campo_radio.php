f3b309d797d8fc49b3fd820f3f5d90f7b10162e3
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-07 10:28:45 -0300

    fixes #108

--- application/models/campo_radio.php
+++ application/models/campo_radio.php
@@ -6,7 +6,7 @@ class CampoRadio extends Campo {
         $display = '<label>' . $this->etiqueta . (in_array('required', $this->validacion) ? '' : ' (Opcional)') . '</label>';
         foreach ($this->datos as $d) {
             $display.='<label class="radio">';
-            $display.='<input ' . ($modo == 'visualizacion' ? 'disabled' : '') . ' type="radio" name="' . $this->nombre . '" value="' . $d->valor . '" ' . ($dato && $d->valor == $dato->valor ? 'checked' : '') . ' />';
+            $display.='<input ' . ($modo == 'visualizacion' || $this->readonly ? 'disabled' : '') . ' type="radio" name="' . $this->nombre . '" value="' . $d->valor . '" ' . ($dato && $d->valor == $dato->valor ? 'checked' : '') . ' />';
             $display.=$d->etiqueta . '</label>';
         }
 
