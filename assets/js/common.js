833a40509aff0f603518840559a580700bdc9525
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-09 12:39:03 -0300

    fixes #33

--- assets/js/common.js
+++ assets/js/common.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
             action: $(el).data("action"),
             onComplete: function(id,filename,respuesta){
                 $parentDiv.find("input[type=hidden]").val(respuesta.file_name);
+                $parentDiv.find(".qq-upload-list").empty();
                 $parentDiv.find(".link").html("<a href='"+site_url+"uploader/datos_get/"+respuesta.file_name+"'>"+respuesta.file_name+"</a> (<a href='#' class='remove'>X</a>)")
             }
         }); 
