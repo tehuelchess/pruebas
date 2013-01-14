3fb61743677d50b208a923598700ab0cacc60b1d
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-14 15:39:23 -0300

    fix #72
    Dentro de la pantalla de seguimiento se puede hacer esta operacion

--- application/views/backend/seguimiento/index.php
+++ application/views/backend/seguimiento/index.php
@@ -1,38 +1,26 @@
 <ul class="breadcrumb">
-    <li class="active">Listado de Trámites</li>
+    <li>
+        Seguimiento de Procesos
+    </li>
 </ul>
+
+
 
 <table class="table">
     <thead>
         <tr>
-            <th>Id</th>
-            <th>Tramite</th>
-            <th>Estado</th>
-            <th>Etapa actual</th>
-            <th>Último cambio</th>
-            <th></th>
+            <th>Proceso</th>
+            <th>Acciones</th>
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($tramites as $t): ?>
-            <tr>
-                <td><?= $t->id ?></td>
-                <td><?= $t->Proceso->nombre ?></td>
-                <td><?= $t->pendiente ? 'En curso' : 'Completado' ?></td>
-                <td>
-                    <?php
-                    $etapas_array = array();
-                    foreach ($t->getEtapasActuales() as $e)
-                        $etapas_array[] = $e->Tarea->nombre;
-                    echo implode(', ', $etapas_array);
-                    ?>
-                </td>
-                <td><?= strftime('%c', mysql_to_unix($t->updated_at)) ?></td>
-                <td>
-                    <a class="btn btn-primary" href="<?= site_url('backend/seguimiento/ver/' . $t->id) ?>"><i class="icon-white icon-eye-open"></i> Seguimiento</a>
-                    <a class="btn btn-danger" href="<?= site_url('backend/seguimiento/borrar/' . $t->id) ?>" onclick="return confirm('¿Esta seguro que desea borrar estre trámite?')"><i class="icon-white icon-trash"></i> Borrar</a>
-                </td>
-            </tr>
+        <?php foreach($procesos as $p): ?>
+        <tr>
+            <td><?=$p->nombre?></td>
+            <td>
+                <a class="btn btn-primary" href="<?=site_url('backend/seguimiento/index_proceso/'.$p->id)?>">Ver trámites</a>
+            </td>
+        </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
---
+++ application/views/backend/seguimiento/index_proceso.php
@@ -0,0 +1,49 @@
+<ul class="breadcrumb">
+    <li><a href="<?= site_url('backend/seguimiento/index') ?>">Seguimiento de Procesos</a></li> <span class="divider">/</span>
+    <li class="active"><?= $proceso->nombre ?></li>
+</ul>
+
+<div class="btn-group pull-right">
+    <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+        Operaciones
+        <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li><a href="<?=site_url('backend/seguimiento/borrar_proceso/'.$proceso->id)?>" onclick="return confirm('¿Esta seguro que desea eliminar todos los tramites de este proceso?')">Borrar todo</a></li>
+    </ul>
+</div>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Tramite</th>
+            <th>Estado</th>
+            <th>Etapa actual</th>
+            <th>Último cambio</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($tramites as $t): ?>
+            <tr>
+                <td><?= $t->id ?></td>
+                <td><?= $t->Proceso->nombre ?></td>
+                <td><?= $t->pendiente ? 'En curso' : 'Completado' ?></td>
+                <td>
+                    <?php
+                    $etapas_array = array();
+                    foreach ($t->getEtapasActuales() as $e)
+                        $etapas_array[] = $e->Tarea->nombre;
+                    echo implode(', ', $etapas_array);
+                    ?>
+                </td>
+                <td><?= strftime('%c', mysql_to_unix($t->updated_at)) ?></td>
+                <td>
+                    <a class="btn btn-primary" href="<?= site_url('backend/seguimiento/ver/' . $t->id) ?>"><i class="icon-white icon-eye-open"></i> Seguimiento</a>
+                    <a class="btn btn-danger" href="<?= site_url('backend/seguimiento/borrar_tramite/' . $t->id) ?>" onclick="return confirm('¿Esta seguro que desea borrar estre trámite?')"><i class="icon-white icon-trash"></i> Borrar</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
