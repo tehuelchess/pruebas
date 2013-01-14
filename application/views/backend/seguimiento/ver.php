a2413b945d7b23f81f3a9d60b7cc37cae6394f0d
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-14 12:49:13 -0300

    fix #73

--- application/views/backend/seguimiento/ver.php
+++ application/views/backend/seguimiento/ver.php
@@ -9,34 +9,40 @@
     });
 </script>
 
+<style>
+    #areaDibujo{
+        width: <?= $tramite->Proceso->width ?>;
+        height: <?= $tramite->Proceso->height ?>;
+    }
+</style>
+
 <ul class="breadcrumb">
     <li>
         <a href="<?= site_url('backend/seguimiento') ?>">Listado de Trámites</a> <span class="divider">/</span>
     </li>
     <li class="active"><?= $tramite->Proceso->nombre ?></li>
 </ul>
-<div class="row-fluid">
-    <div class="span9">
-        <div id="areaDibujo">
-            <h1><?= $tramite->Proceso->nombre ?></h1>
-        </div>  
-    </div>
-    <div class="span3">
-        <div class="well">
-            <h3>Registro de eventos</h3>
-            <hr />
-            <ul>
-                <?php foreach($etapas as $e): ?>
-                <li>
-                    <h4><?=$e->Tarea->nombre?></h4>
-                    <p>Estado: <?=$e->pendiente==0?'Completado':'Pendiente'?></p>
-                    <p><?=$e->created_at?'Inicio: '.strftime('%c',mysql_to_unix($e->created_at)):''?></p>
-                    <p><?=$e->ended_at?'Término: '.strftime('%c',mysql_to_unix($e->ended_at)):''?></p>
-                    <p>Asignado a: <?=!$e->usuario_id?'Ninguno':!$e->Usuario->registrado?'No registrado':$e->Usuario->usuario?></p>
-                    <p><a href="<?=site_url('backend/seguimiento/ver_etapa/'.$e->id)?>">Revisar detalle</a></p> 
-                </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-    </div>
+
+<div class="well" style="position:fixed; top: 200px; right: 20px; width: 300px; height: 500px; z-index: 1000;">
+    <h3>Registro de eventos</h3>
+    <hr />
+    <ul>
+        <?php foreach ($etapas as $e): ?>
+            <li>
+                <h4><?= $e->Tarea->nombre ?></h4>
+                <p>Estado: <?= $e->pendiente == 0 ? 'Completado' : 'Pendiente' ?></p>
+                <p><?= $e->created_at ? 'Inicio: ' . strftime('%c', mysql_to_unix($e->created_at)) : '' ?></p>
+                <p><?= $e->ended_at ? 'Término: ' . strftime('%c', mysql_to_unix($e->ended_at)) : '' ?></p>
+                <p>Asignado a: <?= !$e->usuario_id ? 'Ninguno' : !$e->Usuario->registrado ? 'No registrado' : $e->Usuario->usuario ?></p>
+                <p><a href="<?= site_url('backend/seguimiento/ver_etapa/' . $e->id) ?>">Revisar detalle</a></p> 
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
+
+<div id="areaDibujo">
+    <h1><?= $tramite->Proceso->nombre ?></h1>
+</div>  
+</div>
+
+
