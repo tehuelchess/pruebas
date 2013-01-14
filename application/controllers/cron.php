cf502aa847e400c1d0b04dccb08045eb3af7eadb
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-14 12:37:03 -0300

    fix en backups automaticos

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -37,7 +37,7 @@ class Cron extends CI_Controller {
         $backupName = $this->db->database . '_' . date("Ymd-His") . '.gz';
         $command = $this->config->item('mysqldump_location') . ' -h '.$this->db->hostname.' -u '.$this->db->username.' -p'.$this->db->password.' '.$this->db->database.' | gzip > '.$backupName;
         system($command);
-        system('sshpass -p '.$this->config->item('backupserver_password').' scp ' . $backupName . ' '.$this->config->item('backupserver_username').'@'.$this->config->item('backupserver_password').':'.$this->config->item('backupserver_path'));
+        system('sshpass -p '.$this->config->item('backupserver_password').' scp ' . $backupName . ' '.$this->config->item('backupserver_username').'@'.$this->config->item('backupserver_ip').':'.$this->config->item('backupserver_path'));
         system('rm ' . $backupName);
     }
 
