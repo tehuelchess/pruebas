3eaafa3131932b2a51d81f783f0a1a60c7196541
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-15 15:47:00 -0300

    fix #133

--- application/models/proceso_table.php
+++ application/models/proceso_table.php
@@ -9,7 +9,7 @@ class ProcesoTable extends Doctrine_Table {
                 ->from('Proceso p, p.Cuenta c, p.Tareas t, t.GruposUsuarios g, g.Usuarios u')
                 ->where('t.inicial = 1')
                 //Si el usuario tiene permisos de acceso
-                ->andWhere('(t.acceso_modo="grupos_usuarios" AND u.id = ?) OR (t.acceso_modo = "registrados" AND 1 = ?) OR (t.acceso_modo="publico")',array($usuario->id,$usuario->registrado))
+                ->andWhere('(t.acceso_modo="grupos_usuarios" AND u.id = ?) OR (t.acceso_modo = "registrados") OR (t.acceso_modo="publico")',$usuario->id)
                 //Si la tarea se encuentra activa
                 ->andWhere('1!=(t.activacion="no" OR ( t.activacion="entre_fechas" AND ((t.activacion_inicio IS NOT NULL AND t.activacion_inicio>NOW()) OR (t.activacion_fin IS NOT NULL AND NOW()>t.activacion_fin) )))')
                 ->orderBy('p.id desc');
