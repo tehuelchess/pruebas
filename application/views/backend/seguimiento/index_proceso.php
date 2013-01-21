33aaf4485977765329b3bbfed944f4a93429f8a1
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-21 10:20:53 -0300

    fix #142

--- application/views/backend/seguimiento/index_proceso.php
+++ application/views/backend/seguimiento/index_proceso.php
@@ -9,7 +9,7 @@
         <span class="caret"></span>
     </a>
     <ul class="dropdown-menu">
-        <li><a href="<?=site_url('backend/seguimiento/borrar_proceso/'.$proceso->id)?>" onclick="return confirm('¿Esta seguro que desea eliminar todos los tramites de este proceso?')">Borrar todo</a></li>
+        <li><a href="<?=site_url('backend/seguimiento/borrar_proceso/'.$proceso->id)?>" onclick="if(confirm('¿Esta seguro que desea eliminar todos los tramites de este proceso?'))return confirm('Atención. Esta operación no se podra deshacer y borrara todos los tramites en curso de este proceso. ¿Esta seguro que desea continuar?');else return false;">Borrar todo</a></li>
     </ul>
 </div>
 
