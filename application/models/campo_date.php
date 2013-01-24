c4678f2498dfbab362afed7f4d38e84bda0dbdf0
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-01-24 10:50:31 -0300

    fix #148

--- application/models/campo_date.php
+++ application/models/campo_date.php
@@ -6,7 +6,7 @@ class CampoDate extends Campo{
     
     protected function display($modo, $dato) {
         $display='<label>' . $this->etiqueta . (!$this->readonly && !in_array('required', $this->validacion) ? ' (Opcional)' : '') . '</label>';    
-        $display.='<input '.($this->readonly || $modo=='visualizacion'?'disabled':'').' class="datepicker" ' . ($modo == 'visualizacion' ? 'disabled' : '') . ' type="text" value="' . ($dato?date('d/m/Y',strtotime($dato->valor)):'') . '" />';
+        $display.='<input '.($this->readonly || $modo=='visualizacion'?'disabled':'').' class="datepicker" ' . ($modo == 'visualizacion' ? 'disabled' : '') . ' type="text" value="' . ($dato && $dato->valor?date('d/m/Y',strtotime($dato->valor)):'') . '" />';
         $display.='<input type="hidden" name="'.$this->nombre.'" value="'.($dato?$dato->valor:'').'" />';
         
         return $display;
