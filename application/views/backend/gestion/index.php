f6fb9a122a354b9f59f8771eb2f4e6406dc0f208
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-04-02 16:30:05 -0300

    fix #177

--- application/views/backend/gestion/index.php
+++ application/views/backend/gestion/index.php
@@ -6,8 +6,8 @@
         <div class="btn-group">
             <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-plus icon-white"></i> Nuevo widget <span class="caret"></span></a>
             <ul class="dropdown-menu">
-                <li><a href="<?=site_url('backend/portada/widget_create/tramite_etapas')?>">Tramite por etapas</a></li>
-                <li><a href="<?=site_url('backend/portada/widget_create/tramites_cantidad')?>">Tramites realizados</a></li>
+                <li><a href="<?=site_url('backend/gestion/widget_create/tramite_etapas')?>">Tramite por etapas</a></li>
+                <li><a href="<?=site_url('backend/gestion/widget_create/tramites_cantidad')?>">Tramites realizados</a></li>
             </ul>
         </div>
         </p>
@@ -18,7 +18,7 @@
 
         <?php foreach($widgets as $w):?>
             <div class="widget" data-id="<?=$w->id?>">
-            <?php $data['widget']=$w; $this->load->view('backend/portada/widget_load',$data) ?>
+            <?php $data['widget']=$w; $this->load->view('backend/gestion/widget_load',$data) ?>
             </div>
         <?php endforeach; ?>
     
