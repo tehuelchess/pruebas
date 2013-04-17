414c59a7a63835fdcfcd16b0b3c6155479b444af
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-04-17 10:51:32 -0300

    Se corrige title del backend

--- application/views/backend/template.php
+++ application/views/backend/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Tramitador</title>
+        <title><?=Cuenta::cuentaSegunDominio()?Cuenta::cuentaSegunDominio()->nombre:'SIMPLE'?> - <?= $title ?></title>
 
         <!-- Le styles -->
 
