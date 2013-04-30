79b03410eaae8abc1230f401f4d5bef9cc81eb6e
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-04-30 15:07:55 -0400

    Se previene doble request a los links para iniciar tramites

--- application/views/etapas/inbox.php
+++ application/views/etapas/inbox.php
@@ -17,12 +17,12 @@
         <?php foreach ($etapas as $e): ?>
             <tr>
                 <td><?=$e->Tramite->id?></td>
-                <td class="name"><a href="<?=site_url('etapas/ejecutar/'.$e->id)?>"><?= $e->Tramite->Proceso->nombre ?></a></td>
+                <td class="name"><a class="preventDoubleRequest" href="<?=site_url('etapas/ejecutar/'.$e->id)?>"><?= $e->Tramite->Proceso->nombre ?></a></td>
                 <td><?=$e->Tarea->nombre?></td>
                 <td class="time"><?= strftime('%d.%b.%Y',mysql_to_unix($e->updated_at))?><br /><?= strftime('%T',mysql_to_unix($e->updated_at))?></td>
                 <td><?=$e->getFechaVencimiento()?strftime('%c',$e->getFechaVencimiento()->getTimestamp()):'N/A'?></td>
                 <td class="actions">
-                    <a href="<?=site_url('etapas/ejecutar/'.$e->id)?>" class="btn btn-primary"><i class="icon-edit icon-white"></i> Realizar</a>
+                    <a href="<?=site_url('etapas/ejecutar/'.$e->id)?>" class="btn btn-primary preventDoubleRequest"><i class="icon-edit icon-white"></i> Realizar</a>
                     <?php if($e->netapas==1):?><a href="<?=site_url('tramites/eliminar/'.$e->tramite_id)?>" class="btn" onclick="return confirm('¿Esta seguro que desea eliminar este tramite?')"><i class="icon-trash"></i></a><?php endif ?>
                 </td>
             </tr>
