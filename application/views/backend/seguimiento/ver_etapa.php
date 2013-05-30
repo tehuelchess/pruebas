aeb484ea54fa49c8146e642e5585829522b7bc9d
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-05-30 18:35:05 -0400

    Fix de bug en el seguimiento de etapas

--- application/views/backend/seguimiento/ver_etapa.php
+++ application/views/backend/seguimiento/ver_etapa.php
@@ -45,7 +45,7 @@
                 <?php endforeach ?>
                 <div class="form-actions">
                     <?php if ($paso > 0): ?><a class="btn" href="<?= site_url('backend/seguimiento/ver_etapa/' . $etapa->id . '/' . ($paso - 1)) ?>"><i class="icon-chevron-left"></i> Volver</a><?php endif; ?>
-                    <?php if ($paso + 1 < $etapa->Tarea->Pasos->count()): ?><a class="btn btn-primary" href="<?= site_url('backend/seguimiento/ver_etapa/' . $etapa->id . '/' . ($paso + 1)) ?>">Siguiente</a><?php endif; ?>
+                    <?php if ($paso + 1 < count($etapa->getPasosEjecutables())): ?><a class="btn btn-primary" href="<?= site_url('backend/seguimiento/ver_etapa/' . $etapa->id . '/' . ($paso + 1)) ?>">Siguiente</a><?php endif; ?>
                 </div>
             </fieldset>
         </form>
