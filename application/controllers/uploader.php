db24045b511889e9cd5e2d3a4ce919841626c51f
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-07-15 11:18:05 -0400

    fix #295

--- application/controllers/uploader.php
+++ application/controllers/uploader.php
@@ -26,7 +26,7 @@ class Uploader extends MY_Controller {
         
         
         // list of valid extensions, ex. array("jpeg", "xml", "bmp")
-        $allowedExtensions = array('gif', 'jpg', 'png', 'pdf', 'doc', 'docx','zip','rar');
+        $allowedExtensions = array('gif', 'jpg', 'png', 'pdf', 'doc', 'docx','zip','rar','ppt','pptx','xls','xlsx');
         if(isset($campo->extra->filetypes))
             $allowedExtensions=$campo->extra->filetypes;
             
