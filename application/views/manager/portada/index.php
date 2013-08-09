27f10ac2ffb022a168aa594a6562e59ae2f6bf97
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-08-09 12:28:53 -0400

    Se implementa administracion de cuentas en manager

--- application/views/manager/portada/index.php
+++ application/views/manager/portada/index.php
@@ -2,5 +2,3 @@
     <li class="active"><?=$title?></li>
 </ul>
 
-<p><a href="<?=  site_url('manager/claveunica')?>">Ver estadisticas de uso de ClaveUnica</a></p>
-<p><a href="<?=  site_url('manager/estadisticas')?>">Ver estadisticas completas</a></p>
