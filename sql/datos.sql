38ab237982ffced9ee534034e926d820b6bbd006
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-08-20 12:30:59 -0400

    Se corrige SQL de insercion de datos inicial

--- sql/datos.sql
+++ sql/datos.sql
@@ -1,2 +1,2 @@
 INSERT INTO `cuenta` VALUES(1, 'default');
-INSERT INTO `usuario_backend` VALUES(1, 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Admin', 'Admin', 1);
+INSERT INTO `usuario_backend` VALUES(1, 'admin', 'edd687cf5a62c4c35f5f91f721e55a57ed768e71', 'Admin', 'Admin', 'super', 'r3g4o3ipqjg43qg4r3', 1, NULL);
