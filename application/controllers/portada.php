f806d7487ec7f73d74276e4cf05bbced52c98dff
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-08-22 11:04:00 -0400

    fix #327

--- application/controllers/portada.php
+++ application/controllers/portada.php
@@ -18,5 +18,19 @@ class Portada extends MY_Controller {
         else
             redirect('tramites/disponibles');
     }
+    
+    public function test(){
+        $etapas = Doctrine_Query::create()
+                ->from('Etapa e, e.Tarea t')
+                ->where('e.pendiente = 1 AND t.vencimiento_notificar = 1')
+                ->andWhere('DATEDIFF(e.vencimiento_at,NOW()) <= t.vencimiento_notificar_dias')
+                ->execute();
+        
+        foreach($etapas as $e){
+            print_r($e->toArray());
+            $dias_por_vencer=ceil((strtotime($e->vencimiento_at)-time())/60/60/24);
+            echo 'La etapa "' . $e->Tarea->nombre . '" se encuentra '.($dias_por_vencer>0?'a '.$dias_por_vencer.' días por vencer':'vencida');
+        }
+    }
 
 }
