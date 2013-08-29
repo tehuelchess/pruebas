9c41838c6df854e16484f47596540cd8982f5d33
Nicolas Silva <nsilva@minsegpres.gob.cl>
2013-08-29 12:41:15 -0400

    Se implementan popovers en seguimiento cuando se usa gojs

--- assets/js/seguimiento2.js
+++ assets/js/seguimiento2.js
@@ -1,32 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+});
+
+function drawSeguimiento(actuales, completadas) {
+    $(completadas).each(function(i, el) {
+        var nodedata = diagram.model.findNodeDataForKey(el.identificador);
+        diagram.model.setDataProperty(nodedata, "status", "completed");
+
+        $.get(site_url + "backend/seguimiento/ajax_ver_etapas/" + tramiteId + "/" + el.id, function(d) {
+            $("#draw").append("<div data-id='" + el.id + "' class='popover'><div class='arrow'></div><h3 class='popover-title'>Etapas ejecutadas</h3><div class='popover-content'>" + d + "</div></div>");
+        });
+
+        
+
+    });
+    $(actuales).each(function(i, el) {
+        var nodedata = diagram.model.findNodeDataForKey(el.identificador);
+        diagram.model.setDataProperty(nodedata, "status", "current");
+        
+        $.get(site_url + "backend/seguimiento/ajax_ver_etapas/" + tramiteId + "/" + el.id, function(d) {
+            $("#draw").append("<div data-id='" + el.id + "' class='popover'><div class='arrow'></div><h3 class='popover-title'>Etapas ejecutadas</h3><div class='popover-content'>" + d + "</div></div>");
+        });
+    });
     
+
+
+
+    diagram.addDiagramListener("ObjectSingleClicked", function(e) {
+        var part = e.subject.part;
+        var id;
+        if (!(part instanceof go.Link)) {
+            id = part.data.key;
+            //console.log(part);
+            //part.locationSpot=new go.Spot(1,0.5,0,0);
+            $(".popover[data-id="+id+"]").css("left",part.location.x+200).css("top",part.location.y).toggle();
+        }
     });
 
-function drawSeguimiento(actuales, completadas){
-    $(completadas).each(function(i,el){
-        var nodedata=diagram.model.findNodeDataForKey(el.identificador);
-        diagram.model.setDataProperty(nodedata,"status","completed");
-        
-        //var node=diagram.findNodeForKey(el.identificador);
-        //console.log(node.location.x);
-    });
-    $(actuales).each(function(i,el){
-        var nodedata=diagram.model.findNodeDataForKey(el.identificador);
-        diagram.model.setDataProperty(nodedata,"status","current");  
-    });
-    
+
     /*
-    $('#draw .box.actual,#draw .box.completado').each(
-        function(){
-            var el=this;
-            $.get(site_url+"backend/seguimiento/ajax_ver_etapas/"+tramiteId+"/"+el.id,function(d){
-                $(el).unbind('hover').popover({
-                    html: true,
-                    title: "Etapas ejecutadas",
-                    content: d
-                });
-            });
-        });
-        */
+     $('#draw .box.actual,#draw .box.completado').each(
+     function(){
+     var el=this;
+     $.get(site_url+"backend/seguimiento/ajax_ver_etapas/"+tramiteId+"/"+el.id,function(d){
+     $(el).unbind('hover').popover({
+     html: true,
+     title: "Etapas ejecutadas",
+     content: d
+     });
+     });
+     });
+     */
 
 }
