1cc4db5af6fdd1d28528e7ed9ff8548b9d235463
Nicolas Silva <nsilva@minsegpres.gob.cl>
2015-01-30 10:36:38 -0300

    Se incluye opcion de deshabilitar ultima pantalla de confirmacion

--- application/models/tarea.php
+++ application/models/tarea.php
@@ -28,6 +28,7 @@ class Tarea extends Doctrine_Record {
         $this->hasColumn('vencimiento_notificar');          //Indica si se debe notificar en caso de que se acerque la fecha de vencimiento
         $this->hasColumn('vencimiento_notificar_dias');     //Indica desde cuantos dias de anticipacion se debe notificar la fecha de vencimiento
         $this->hasColumn('vencimiento_notificar_email');    //Cual es el email donde se debe notificar
+        $this->hasColumn('paso_confirmacion');              //Boolean que indica si se debe incorporar una ultima pantalla de confirmacion antes de avanzar la tarea
         
     }
 
