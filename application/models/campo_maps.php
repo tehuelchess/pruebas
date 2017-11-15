445be26168bca55cf1455f793066a5da9dc7926c
Jimmy M <jmunodu@gmail.com>
2017-11-15 14:32:47 -0300

    Feature/usabilidad sprint 3 (#641)
    * Se agrega archivos de log a gitignore
    
    * integración google recaptcha, eliminación logica de procesos, enlaces hacia ayuda
    
    * se elimina ejemplos de key para token de captcha
    
    * Integra mapa en formulario backend
    
    * se mejora integración de mapa
    
    * se corrige carga de multiples mapas en formularios, se corrige visualización de mapa en documentos
    
    * Se corrigen observaciones en mapa levantadas en reunión de seguimiento, además se corrige geolocalización.
    
    * Se integra atributo de ambiente y vinculación de cuentas
    
    * Se resuelve issue en administrador de cuentas
    
    * Elimina la capacidad de crear nuevos usuarios en la interfaz que utilizan los ciudadanos
    
    * Elimina la capacidad de crear nuevos usuarios en la interfaz que utilizan los ciudadanos
    
    * se integra requerimiento 'Obtener datos del usuario desde clave única'
    
    * Se integra validación de reglas
    
    * se corrige problema en vinculación de cuentas al tratar de vincular una cuenta nueva directamente
    
    * Se agrega ejemplo en config.php.sample de como debe ir variable de maps.
    
    * se cambia version de scope para clave unica
    
    * Se agrega event on enter para input de mapa

--- application/models/campo_maps.php
+++ application/models/campo_maps.php
@@ -117,6 +117,44 @@ class CampoMaps extends Campo {
                     }
         ';
 
+        if ($modo == 'edicion') {
+            $display .= '
+            var pac_input = document.getElementById(' . $this->id . ');
+
+            (function pacSelectFirst(input) {
+                // store the original event binding function
+                var _addEventListener = (input.addEventListener) ? input.addEventListener : input.attachEvent;
+
+                function addEventListenerWrapper(type, listener) {
+                    // Simulate a down arrow keypress on hitting return when no pac suggestion is selected,
+                    // and then trigger the original listener.
+                    if (type == "keydown") {
+                        var orig_listener = listener;
+                        listener = function(event) {
+                            var suggestion_selected = $(".pac-item-selected").length > 0;
+                            if (event.which == 13 && !suggestion_selected) {
+                                event.preventDefault();
+                                var simulated_downarrow = $.Event("keydown", {
+                                    keyCode: 40,
+                                    which: 40
+                                });
+                                orig_listener.apply(input, [simulated_downarrow]);
+                            }
+                            orig_listener.apply(input, [event]);
+                        };
+                    }
+                    _addEventListener.apply(input, [type, listener]);
+                }
+
+                input.addEventListener = addEventListenerWrapper;
+                input.attachEvent = addEventListenerWrapper;
+
+                var autocomplete = new google.maps.places.Autocomplete(input);
+                return false;
+            })(pac_input);
+            ';
+        }
+
         if ($columns) {
             log_message('debug', 'columns: ' . json_encode($columns));
             $i = 0;
